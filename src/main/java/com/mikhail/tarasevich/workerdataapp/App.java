61f77932286b5c862d08c8b7f0a359061fb5ad35
mikhailtarasevich <tarasevichmikhail93@gmail.com>
2023-07-11 16:47:26 +0300

    init commit

---
+++ src/main/java/com/mikhail/tarasevich/workerdataapp/App.java
@@ -0,0 +1,13 @@
+package com.mikhail.tarasevich.workerdataapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
